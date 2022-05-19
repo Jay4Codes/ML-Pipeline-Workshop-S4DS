92b247ec96b9a1510ffc1d3eeabb36e80c62528d
Jay Jain <jay4codes@gmail.com>
2022-05-19 20:46:12 +0530

    content addons

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+
+""" from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name) """
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
